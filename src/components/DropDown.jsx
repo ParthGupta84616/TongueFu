8156f40d679c10392add075801219a9d33b56fb0
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-18 22:02:37 +0530

    Update DropDown.jsx

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -32,7 +32,7 @@ const FlyoutLink = ({ children, href, FlyoutContent }) => {
           style={{
             transform: showFlyout ? "scaleX(1)" : "scaleX(0)",
           }}
-          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-indigo-300 transition-transform duration-300 ease-out"
+          className="absolute -bottom-2 -left-2 -right-2 h-1 origin-left scale-x-0 rounded-full bg-slate-800 transition-transform duration-300 ease-out"
         />
       </a>
       <AnimatePresence>
@@ -43,11 +43,11 @@ const FlyoutLink = ({ children, href, FlyoutContent }) => {
             exit={{ opacity: 0, y: 15 }}
             // style={{  }}
             transition={{ duration: 0.3, ease: "easeOut" }}
-            className="absolute  top-12 bg-white text-black"
+            className="absolute  top-12 text-slate-300"
             style ={{ width : "700px" }}
           >
             <div className="absolute -top-6 left-0 right-0 h-6 bg-transparent" />
-            <div className="absolute left-10 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white" />
+            <div className="absolute left-10 top-0 h-4 w-4 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-slate-800" />
             <FlyoutContent />
           </motion.div>
         )}
@@ -58,8 +58,8 @@ const FlyoutLink = ({ children, href, FlyoutContent }) => {
 
 const PricingContent = () => {
   return (
-      <div className="flex p-5 bg-white shadow-xl ">
-        <div className="w-4/5 mr-5 text-neutral-800">
+      <div className="flex p-5 bg-slate-800 rounded-xl shadow-xl ">
+        <div className="w-4/5 mr-5 text-neutral-200">
             <h1>Blogs & Articles</h1>
             <div className="card flex h-2/5 w-full p-2">
                 <div className="img p-2 bg-slate-950 w-1/4 rounded-xl">
