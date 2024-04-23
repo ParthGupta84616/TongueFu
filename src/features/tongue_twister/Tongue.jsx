3a7a6b0b0ea7a9a2ae2f6375724ce6d81c73b63b
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-23 23:53:26 +0530

    Update Tongue.jsx

--- src/features/tongue_twister/Tongue.jsx
+++ src/features/tongue_twister/Tongue.jsx
@@ -17,12 +17,15 @@ const Tongue = () => {
   const [language, setLanguage] = useState(null)
   const [level, setLevel] = useState(null)
   const dispatch = useDispatch();
+  
   const Twister = useSelector(selectTwisterEnglish);
   const [showTwister, setShowTwister] = useState(false);
   const color = useMotionValue(COLORS_TOP[0]);
   const [showWindow, setShowWindow] = useState(1)
-  const [randomNumber, setRandomNumber] = useState(1)
-
+  const [randomNumber, setRandomNumber] = useState(null)
+  const [line, setLine] = useState(null)
+  const [showLine, setShowLine] = useState(null)
+  const [word, setWord] = useState(null)
   useEffect(() => {
     animate(color, COLORS_TOP, {
       ease: "easeInOut",
@@ -51,23 +54,33 @@ const Tongue = () => {
   }, [dispatch, language, level]);
   
   useEffect(() => {
-   
+    if(Twister[1]?.text){
+      const randomNumber = Math.floor(Math.random() * Twister.length);
+      const randomText = Twister[randomNumber].text;
 
-  if(showTwister && Twister){
-    setRandomNumber(Math.floor(Math.random() * Twister.length) + 1)
+    setShowLine(randomText);
+    setLine(randomText.split(" "));
+    }
+    }, [Twister[1]?.text])
+    const handleword= (item) => {
+      setWord(item)
+      // console.log(word)
+    }
+    useEffect(() => {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); 
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+    }, [word])
     
-
-  }
-  }, [Twister?.text])
-  
-  // let line = Twister[randomNumber].text
-
-  // let lineArr = line.split(" ")
-
 
   return (
     <div>
-    <div className="relative h-screen w-full">
+    <div className="relative " style={{minHeight:"45rem"}}>
       <motion.section
       style={{
         backgroundImage,
@@ -80,9 +93,9 @@ const Tongue = () => {
         </Canvas>
       </div>
     </motion.section>
-    <div className="relative z-20 text-white flex justify-center h-screen w-full lg:p-10">
-  <div className="upperbox lg:w-1/2 w-4/5 flex flex-col justify-center items-center"> 
-    <div className="text-6xl text-gray-100 font-bold p-10 text-center">
+    <div className="relative z-20 text-white flex justify-center lg:p-10">
+  <div className="upperbox lg:w-1/2 w-full flex flex-col justify-center items-center"> 
+    <div className="lg:text-6xl text-xl text-gray-100 font-bold p-10 text-center">
       Tongue Twister
     </div>
     <div className="text-2xl text-gray-300 font-semibold p-10 text-center "> 
@@ -144,36 +157,47 @@ const Tongue = () => {
         </div>
       ):showWindow===3?(
         <div className="w-full">
-          <div class=" flex text-blue-950 rounded-full border-gray-900 opacity-80 bg-gray-500 border-4 m-10 p-5 font-mono  text-center justify-center text-3xl">
+          <div class=" flex text-blue-950 rounded-2xl border-gray-900 opacity-80 bg-gray-500 border-4 m-10 p-5 font-mono  text-center justify-center text-3xl">
      <h1> 
      {!showTwister && (
         <div>Loading</div>
       )}
       {showTwister && Twister && Twister[1] && (
-        <div>{Twister[randomNumber].text}</div>
+        <div>{showLine}</div>
 
       )}
       </h1>
     </div>
     <div className="flex justify-center items-center">
-    <button className="rounded-2xl border-2 border-dashed border-black  bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none" 
+      <button className="rounded-2xl border-2 border-dashed border-black  bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none" 
             onClick={() => {
               setShowWindow(1);
             }}>
               Regenerate
-            </button>
-    
+        </button>
     </div>
-
+          {line && (
+            <div className="button-container flex flex-wrap justify-center items-center mt-8 p-4">
+            {line.map((item, index) => (
+            <div key={index} className='flex'>
+              <button 
+              onClick={() => handleword(item)}
+              type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{item} </button>
+            </div>
+          ))}
+            </div>
+          )}
+          
+          
+          {/* <audio controls>
+  <source src="//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3" type="audio/mpeg" />
+  Your browser does not support the audio element.
+</audio> */}
         </div>
       ):(
         <h2>Error</h2>
       )
       }
-      
-    
-
-
   </div>
 </div>
 
